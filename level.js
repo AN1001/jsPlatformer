0a5ed26291b8635d0b5725a93718a237599fd748
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2024-04-26 19:43:17 +0100

    Update level.js

--- level.js
+++ level.js
@@ -1,193 +1,311 @@
-const data = [
-    {
-        "x": 50,
-        "y": 32,
-        "width": 30,
-        "height": 30,
-        "type": "barrel",
-    },
-    {
-        "x": 15,
-        "y": 32,
-        "width": 30,
-        "height": 30,
-        "type": "barrel",
-    },
-    {
-        "x": 200,
-        "y": 60,
-        "width": 60,
-        "height": 20,
-        "type": "platform2",
-    },
-    {
-        "x": 300,
-        "y": 60,
-        "width": 60,
-        "height": 20,
-        "type": "platformRusted",
-    },
-    {
-        "x": 200,
-        "y": 120,
-        "width": 60,
-        "height": 20,
-        "type": "platform",
-    },
-    {
-        "x": 200,
-        "y": 180,
-        "width": 60,
-        "height": 20,
-        "type": "platform2",
-    },
-    {
-        "x": 200,
-        "y": 240,
-        "width": 60,
-        "height": 20,
-        "type": "platformRusted",
-    },
-    {
-        "x": 390,
-        "y": 240,
-        "width": 60,
-        "height": 20,
-        "type": "platform",
-    },
-    {
-        "x": 580,
-        "y": 240,
-        "width": 60,
-        "height": 20,
-        "type": "platformRusted",
-    },
-    {
-        "x": 780,
-        "y": 240,
-        "width": 60,
-        "height": 20,
-        "type": "platformRusted",
-    },
-    {
-        "x": 780,
-        "y": 150,
-        "width": 140,
-        "height": 20,
-        "type": "platform",
-    },
-    {
-        "x": 880,
-        "y": 190,
-        "width": 40,
-        "height": 20,
-        "type": "platform",
-    },
-    {
-        "x": 880,
-        "y": 300,
-        "width": 40,
-        "height": 20,
-        "type": "platform2",
-    },
-    {
-        "x": 0,
-        "y": 400,
-        "width": 800,
-        "height": 40,
-        "type": "platform",
-    },
-    {
-        "x": 780,
-        "y": 350,
-        "width": 60,
-        "height": 20,
-        "type": "platformRusted",
-    },
-    {
-        "x": 300,
-        "y": 450,
-        "width": 30,
-        "height": 30,
-        "type": "repel"
-    },
-    {
-        "x": 100,
-        "y": 450,
-        "width": 60,
-        "height": 20,
-        "type": "bounce",
-        "bouncyness":1.8
-    },
-    {
-        "x": 100,
-        "y": 650,
-        "width": 60,
-        "height": 20,
-        "type": "bounce",
-        "bouncyness":1.8
-    },
-    {
-        "x": 100,
-        "y": 850,
-        "width": 60,
-        "height": 20,
-        "type": "bounce",
-        "bouncyness":1.8
-    },
-    {
-        "x": 350,
-        "y": 850,
-        "width": 60,
-        "height": 20,
-        "type": "bounce",
-        "bouncyness":1.8
-    },
-    {
-        "x": 450,
-        "y": 430,
-        "width": 30,
-        "height": 30,
-        "type": "env",
-        "code":"signL"
-    },
-    {
-        "x": 600,
-        "y": 850,
-        "width": 60,
-        "height": 20,
-        "type": "bounce",
-        "bouncyness":1.8
-    },
-    {
-        "x": 600,
-        "y": 1050,
-        "width": 30,
-        "height": 30,
-        "type": "repel"
-    },
-    {
-        "x": 700,
-        "y": 1100,
-        "width": 60,
-        "height": 20,
-        "type": "bounce",
-        "bouncyness":2.8
-    },
-    {
-        "x": 700,
-        "y": 1081,
-        "width": 320,
-        "height": 40,
-        "type": "platform2"
-    },
-    {
-        "x": 900,
-        "y": 1111,
-        "width": 30,
-        "height": 30,
-        "type": "barrel"
-    }
+const data = [{
+		"x": 50,
+		"y": 32,
+		"width": 30,
+		"height": 30,
+		"type": "barrel"
+	},
+	{
+		"x": 15,
+		"y": 32,
+		"width": 30,
+		"height": 30,
+		"type": "barrel"
+	},
+	{
+		"x": 200,
+		"y": 60,
+		"width": 60,
+		"height": 20,
+		"type": "platform2"
+	},
+	{
+		"x": 300,
+		"y": 60,
+		"width": 60,
+		"height": 20,
+		"type": "platformRusted"
+	},
+	{
+		"x": 200,
+		"y": 120,
+		"width": 60,
+		"height": 20,
+		"type": "platform"
+	},
+	{
+		"x": 200,
+		"y": 180,
+		"width": 60,
+		"height": 20,
+		"type": "platform2"
+	},
+	{
+		"x": 200,
+		"y": 240,
+		"width": 60,
+		"height": 20,
+		"type": "platformRusted"
+	},
+	{
+		"x": 390,
+		"y": 240,
+		"width": 60,
+		"height": 20,
+		"type": "platform"
+	},
+	{
+		"x": 580,
+		"y": 240,
+		"width": 60,
+		"height": 20,
+		"type": "platformRusted"
+	},
+	{
+		"x": 780,
+		"y": 240,
+		"width": 60,
+		"height": 20,
+		"type": "platformRusted"
+	},
+	{
+		"x": 780,
+		"y": 150,
+		"width": 140,
+		"height": 20,
+		"type": "platform"
+	},
+	{
+		"x": 880,
+		"y": 190,
+		"width": 40,
+		"height": 20,
+		"type": "platform"
+	},
+	{
+		"x": 880,
+		"y": 300,
+		"width": 40,
+		"height": 20,
+		"type": "platform2"
+	},
+	{
+		"x": 0,
+		"y": 400,
+		"width": 800,
+		"height": 40,
+		"type": "platform"
+	},
+	{
+		"x": 780,
+		"y": 350,
+		"width": 60,
+		"height": 20,
+		"type": "platformRusted"
+	},
+	{
+		"x": 300,
+		"y": 450,
+		"width": 30,
+		"height": 30,
+		"type": "repel"
+	},
+	{
+		"x": 100,
+		"y": 450,
+		"width": 60,
+		"height": 20,
+		"type": "bounce",
+		"bouncyness": 1.8
+	},
+	{
+		"x": 100,
+		"y": 650,
+		"width": 60,
+		"height": 20,
+		"type": "bounce",
+		"bouncyness": 1.8
+	},
+	{
+		"x": 100,
+		"y": 850,
+		"width": 60,
+		"height": 20,
+		"type": "bounce",
+		"bouncyness": 1.8
+	},
+	{
+		"x": 350,
+		"y": 850,
+		"width": 60,
+		"height": 20,
+		"type": "bounce",
+		"bouncyness": 1.8
+	},
+	{
+		"x": 450,
+		"y": 430,
+		"width": 30,
+		"height": 30,
+		"type": "env",
+		"code": "signL"
+	},
+	{
+		"x": 600,
+		"y": 850,
+		"width": 60,
+		"height": 20,
+		"type": "bounce",
+		"bouncyness": 1.8
+	},
+	{
+		"x": 600,
+		"y": 1050,
+		"width": 30,
+		"height": 30,
+		"type": "repel"
+	},
+	{
+		"x": 700,
+		"y": 1100,
+		"width": 60,
+		"height": 20,
+		"type": "bounce",
+		"bouncyness": 2.8
+	},
+	{
+		"x": 700,
+		"y": 1081,
+		"width": 320,
+		"height": 40,
+		"type": "platform2"
+	},
+	{
+		"x": 900,
+		"y": 1111,
+		"width": 30,
+		"height": 30,
+		"type": "barrel",
+      	"chatGpt":"From here on"
+	},
+	{
+		"x": 980, 
+		"y": 32,
+		"width": 30,
+		"height": 30,
+		"type": "barrel"
+	},
+	{
+		"x": 945,
+		"y": 32,
+		"width": 30,
+		"height": 30,
+		"type": "barrel"
+	},
+	{
+		"x": 1130, 
+		"y": 60,
+		"width": 60,
+		"height": 20,
+		"type": "platform2"
+	},
+	{
+		"x": 1230, 
+		"y": 60,
+		"width": 60,
+		"height": 20,
+		"type": "platformRusted"
+	},
+	{
+		"x": 1130, 
+		"y": 120,
+		"width": 60,
+		"height": 20,
+		"type": "platform"
+	},
+	{
+		"x": 1710, 
+		"y": 350,
+		"width": 60,
+		"height": 20,
+		"type": "platformRusted"
+	},
+	{
+		"x": 1230,
+		"y": 450,
+		"width": 30,
+		"height": 30,
+		"type": "repel"
+	},
+	{
+		"x": 1030, 
+		"y": 450,
+		"width": 60,
+		"height": 20,
+		"type": "bounce",
+		"bouncyness": 1.8
+	},
+	{
+		"x": 1030, 
+		"y": 650,
+		"width": 60,
+		"height": 20,
+		"type": "bounce",
+		"bouncyness": 1.8
+	},
+	{
+		"x": 1030, 
+		"y": 850,
+		"width": 60,
+		"height": 20,
+		"type": "bounce",
+		"bouncyness": 1.8
+	},
+	{
+		"x": 1280, 
+		"y": 850,
+		"width": 60,
+		"height": 20,
+		"type": "bounce",
+		"bouncyness": 1.8
+	},
+	{
+		"x": 1530, 
+		"y": 850,
+		"width": 60,
+		"height": 20,
+		"type": "bounce",
+		"bouncyness": 1.8
+	},
+	{
+		"x": 1530, 
+		"y": 1050,
+		"width": 30,
+		"height": 30,
+		"type": "repel"
+	},
+	{
+		"x": 1630, 
+		"y": 1100,
+		"width": 60,
+		"height": 20,
+		"type": "bounce",
+		"bouncyness": 2.8
+	},
+	{
+		"x": 1630, 
+		"y": 1081,
+		"width": 320,
+		"height": 40,
+		"type": "platform2"
+	},
+	{
+		"x": 1830,
+		"y": 1111,
+		"width": 30,
+		"height": 30,
+		"type": "barrel"
+	}
 ]
 
 export default data
